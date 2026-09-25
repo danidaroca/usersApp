cca39695e86910ac689b8a6d51df9254701796e9
nombre <dani.daroca@gmail.com>
2026-09-25 10:06:32 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
+  
+  // 👉 AÑADE ESTE NUEVO BLOQUE PARA TU PÁGINA:
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+    path: 'users',
+    loadComponent: () => import('./pages/users/users.page').then(m => m.UsersPage)
+  }
 ];
